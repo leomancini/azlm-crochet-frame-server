fc068986ca86466ecbb48f14e656bf87a274f13b
Leo Mancini <mancini@leo.gd>
2025-03-29 18:08:35 -0400

    Adding sample data

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3102;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.get("/", (req, res) => {
+  res.json({
+    company: "OFC",
+    founded: 2022,
+    fun: true,
+    primes: [2, 3, 5],
+    pi: 3.14,
+    mixed: [false, null, 3, true, 2.7, "cheese"]
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
